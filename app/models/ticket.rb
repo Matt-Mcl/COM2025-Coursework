743d0023b12074d62696b190d92c391e72ffd716
mm02417 <mm02417@surrey.ac.uk>
2020-11-29 11:32:52 +0000

    Added derived space_id from inputting carpark when creating ticket. Currently just picks first space. Spaces need to be picked by availability instead.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,8 +1,37 @@
 class Ticket < ApplicationRecord
-  belongs_to :space
-  validates :space, :registration_number, :paid_from, :paid_to, presence: true
-
+  before_save :find_space_id
   before_save :calculate_total_cost
+
+  belongs_to :space, optional: true
+  validates :registration_number, :paid_from, :paid_to, presence: true
+
+  <<-TESTS
+    puts '&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&'
+    puts space_id
+    puts space
+    @ticket_carpark = Carpark.find(space_id)
+    @spaces = @ticket_carpark.spaces
+    puts '^^^^^^^^^^^^'
+    puts @spaces
+    puts '^^^^^^^^^^^^'
+    puts @spaces.where(space_number: 7)
+    @new_id = @spaces.where(space_number: 7)
+    puts @new_id.ids
+    # self.space_id = @spaces.where(space_number: 23)
+    # Ticket.where(id: self.id).update_all(space_id: @new_id.ids)
+    # Ticket.where(id: self.id).update_all(['space_id = ?', @new_id.ids])
+    @new_space_id = @new_id.ids.to_s.gsub(/\D/, '').to_i
+    puts @new_space_id
+    self.space_id = @new_space_id
+    puts space_id
+    puts '&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&'
+  TESTS
+  def find_space_id
+    @ticket_carpark = Carpark.find(space_id)
+    @spaces = @ticket_carpark.spaces
+    @new_space_id = @spaces.where(space_number: 1)
+    self.space_id = @new_space_id.ids.to_s.gsub(/\D/, '').to_i
+  end
 
   <<-TESTS
     puts '---------------------------------------------------------------------'
@@ -19,4 +48,8 @@ class Ticket < ApplicationRecord
     @ticket_space = Space.find(space_id)
     self.total_cost = ((end_time - start_time) / 3600.0) * @ticket_space.hourly_cost
   end
+
+
+
+
 end
